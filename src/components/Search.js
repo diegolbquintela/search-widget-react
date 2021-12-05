9fc7ca48fc09e709dbea6d81cb70a7968077f74c
Diego Quintela <diegolucasq@gmail.com>
2021-12-05 18:48:51 -0500

    added get request for user input

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Search = () => {
   const [searchTerm, setSearchTerm] = useState("");
@@ -6,6 +7,22 @@ const Search = () => {
   const searchHandler = (event) => {
     setSearchTerm(event.target.value);
   };
+
+  useEffect(() => {
+    const search = async () => {
+      await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: searchTerm,
+        },
+      });
+    };
+
+    search();
+  }, [searchTerm]);
 
   return (
     <div>
